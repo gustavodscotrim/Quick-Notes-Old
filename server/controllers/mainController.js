02b161871fc1609f7a2d8f27ae7ff98966eb6820
Gustavo Cotrim <gustavodscotrim@outlook.com>
2023-09-25 11:06:41 -0300

    Release candidate

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -1,25 +1,25 @@
 
 // GET Homepage
 
-exports.homepage = async(req, res) => {
+exports.homepage = async (req, res) => {
     const locals = {
         title: "Quick Notes",
         description: "Note Web Application"
     }
-    
+
     res.render("index", {
         locals,
         layout: "../views/layouts/front-page"
-        });
+    });
 }
 
 // GET About
 
-exports.about = async(req, res) => {
+exports.about = async (req, res) => {
     const locals = {
         title: "About - Quick Notes",
         description: "Note Web Application"
     }
-    
+
     res.render("about", locals);
 }
